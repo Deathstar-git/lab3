6418e2b35d4db434917d1e33e1c9a60a1be1c34f
Deathstar-git <79025632112tch@gmail.com>
2019-09-26 13:54:29 +0800

    start lab version

--- lab3.py
+++ lab3.py
@@ -3,7 +3,6 @@ def main():
     age = (10, 23, 17)
     height = (165, 157, 178)
     a = [name, age, height]
-    sorted(a)
     for i in range(len(a)):
         print("Name:" + str(a[0][i]) + " Age:" + str(a[1][i]) + " Height:" + str(a[2][i]))
 
