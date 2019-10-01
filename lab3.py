4da756c78023c06b4e5768e4a9bb078a16d5a527
Deathstar-git <79025632112tch@gmail.com>
2019-10-01 12:19:29 +0800

    start lab version

--- lab3.py
+++ lab3.py
@@ -1,10 +1,13 @@
 def main():
-    name = ('Yen', 'Jack', 'Mary')
-    age = (10, 23, 17)
-    height = (165, 157, 178)
+    name = ['Yen', 'Jack', 'Mary']
+    name.sort()
+    age = [10, 23, 17]
+    age.sort()
+    height = [165, 196, 180]
+    height.sort()
     a = [name, age, height]
     for i in range(len(a)):
-        print("Name:" + str(a[0][i]) + " Age:" + str(a[1][i]) + " Height:" + str(a[2][i]))
+        print("Name: " + str(a[0][i]) + " Age: " + str(a[1][i]) + " Height: " + str(a[2][i]))
 
 
 main()
