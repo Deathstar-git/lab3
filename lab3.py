3401539ee48ec866e64752b4cee264040b07b325
Deathstar-git <79025632112tch@gmail.com>
2019-10-01 18:02:33 +0800

    start lab version

--- lab3.py
+++ lab3.py
@@ -1,13 +1,20 @@
 def main():
-    name = ['Yen', 'Jack', 'Mary']
-    name.sort()
-    age = [10, 23, 17]
-    age.sort()
-    height = [165, 196, 180]
-    height.sort()
-    a = [name, age, height]
-    for i in range(len(a)):
-        print("Name: " + str(a[0][i]) + " Age: " + str(a[1][i]) + " Height: " + str(a[2][i]))
+    i = input("Введите кол-во людей: ")
+    try:
+        int(i)
+    except ValueError:
+        print("Введено неверное значение!")
+
+    if i.isdigit:
+        name = ['Yen', 'Jack', 'Mary']
+        name.sort()
+        age = [10, 23, 17]
+        age.sort()
+        height = [165, 196, 180]
+        height.sort()
+        a = [name, age, height]
+        for i in range(len(a)):
+            print("Name: " + str(a[0][i]) + " Age: " + str(a[1][i]) + " Height: " + str(a[2][i]))
 
 
 main()
