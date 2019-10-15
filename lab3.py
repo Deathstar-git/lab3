d6fafb99a8f4bd24cffb750c879bbb72957ac04d
Deathstar-git <79025632112tch@gmail.com>
2019-10-15 16:00:43 +0800

    Working lab version with priority and CLI

--- lab3.py
+++ lab3.py
@@ -4,17 +4,23 @@ import argparse
 def create_parser():
     p = argparse.ArgumentParser()
     p.add_argument('i', type=int)
+    p.add_argument('j', type=int)
+    p.add_argument('k', type=int)
 
     return p
 
 
-def main(i):
-    choose_sort = {0: 'Сортировка по имени:', 1: 'Сортировка по росту:', 2: 'Сортировка по возрасту:'}
-    print(str(choose_sort[i]))
+def main(i, j, k):
+    choose_sort = {0: 'Сортировка по имени', 1: 'Сортировка по росту', 2: 'Сортировка по возрасту'}
+    print('1:' + str(choose_sort[i]))
+    print('2:' + str(choose_sort[j]))
+    print('3:' + str(choose_sort[k]))
     lst = [('Vanya', 172.5, 17), ('Petya', 186.0, 20), ('Anya', 168.3, 16),
            ('Masha', 178.7, 19), ('Oleg', 168.3, 17), ('Kolya', 180.4, 18), ('Natasha', 175.0, 20),
            ('Boris', 159.8, 16), ('Galina', 157.3, 18), ('Maks', 177.0, 19)]
     lst = sorted(lst, key=lambda tpl: tpl[i])
+    lst = sorted(lst, key=lambda tpl: tpl[j])
+    lst = sorted(lst, key=lambda tpl: tpl[k])
     for j in range(len(lst)):
         print(lst[j])
     sum_r = 0
@@ -43,4 +49,4 @@ def main(i):
 if __name__ == '__main__':
     parser = create_parser()
     namespace = parser.parse_args()
-    main(namespace.i)
+    main(namespace.i, namespace.j, namespace.k)
